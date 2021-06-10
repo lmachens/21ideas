c1994361a1979376bf0c80692e22210aaa53b6f3
J-Bannach <JohannaBannach@gmail.com>
2021-06-10 10:07:39 +0200

    :ambulance: Forward client routing in production

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,8 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config();
 
 const { PORT = 3020 } = process.env;
 const app = express();
@@ -11,4 +15,9 @@ app.use(express.static('dist/app'));
 
 app.listen(PORT, () => {
   console.log(`Boilerplate listening at http://localhost:${PORT}`);
+});
+
+// Handle client routing, return all requests to the app
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, 'app/index.html'));
 });
