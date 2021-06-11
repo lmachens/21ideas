417d531052049719479542d6394eaf3347e08228
J-Bannach <JohannaBannach@gmail.com>
2021-06-11 17:07:40 +0200

    ✅ Test API fetch

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -1,8 +1,20 @@
 import express from 'express';
+import getRecipeByName from './search';
 
 const router = express.Router();
 
-router.get('/search', (_req, res) => {
-  res.status(404).send('Hello world');
+router.get('/search', async (_req, res, next) => {
+  try {
+    const { query } = _req;
+    if (typeof query.recipename !== 'string') {
+      res.status(404).send('Hello world!');
+      return;
+    }
+    const result = await getRecipeByName(query.recipename);
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
 });
+
 export default router;
