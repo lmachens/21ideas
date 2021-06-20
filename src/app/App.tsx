e8bc5a60a2166b6fbc6849700b88f389c77f7e42
J-Bannach <JohannaBannach@gmail.com>
2021-06-20 13:12:59 +0200

    🎨 Adapt App.tsx (WIP)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import styles from './App.module.css';
 import placeholder from './components/SearchField/SearchField';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
@@ -11,40 +10,14 @@ function App(): JSX.Element {
     <Router>
       <div className={styles.App}>
         <header className={styles['App-header']}>
-          <img src={logo} className={styles['App-logo']} alt="logo" />
-
           <button onClick={() => alert('Hello World')}>Test me</button>
 
           <label className={styles.label}>
             <input className={styles.label__input} placeholder={placeholder} />
           </label>
-
-          <p>
-            <a
-              className={styles['App-link']}
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-            {' | '}
-            <a
-              className={styles['App-link']}
-              href="https://vitejs.dev/guide/features.html"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Vite Docs
-            </a>
-          </p>
           <Switch>
-            <Route path="/about">
-              <main>About</main>
-            </Route>
-            <Route path="/">
-              <main>Home</main>
-            </Route>
+            <Route path="/search"></Route>
+            <Route path="/"></Route>
           </Switch>
         </header>
       </div>
