4791a5138a199552d8a0abdf6d1b368c976c2b75
J-Bannach <JohannaBannach@gmail.com>
2021-06-23 08:55:10 +0200

    ✨ Add Back Icon

---
+++ src/app/components/Icons/BackIcon.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function BackIcon(): JSX.Element {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      data-name="Layer 1"
+      viewBox="0 0 24 24"
+      width="25"
+      height="25"
+    >
+      <path
+        fill="var(--text-color)"
+        d="M17,11H9.41l3.3-3.29a1,1,0,1,0-1.42-1.42l-5,5a1,1,0,0,0-.21.33,1,1,0,0,0,0,.76,1,1,0,0,0,.21.33l5,5a1,1,0,0,0,1.42,0,1,1,0,0,0,0-1.42L9.41,13H17a1,1,0,0,0,0-2Z"
+      />
+    </svg>
+  );
+}
+
+export default BackIcon;
