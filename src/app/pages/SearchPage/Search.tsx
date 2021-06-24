8b96c1f15d0b4367fd23e392908b0119ce2400e0
J-Bannach <JohannaBannach@gmail.com>
2021-06-24 16:22:26 +0200

    ✨ Add Search page

---
+++ src/app/pages/SearchPage/Search.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import NavBar from '../../components/NavBar/NavBar';
+import styles from './Search.module.css';
+import MealSearch from '../../components/MealSearch/MealSearch';
+
+type Meals = {
+  meals: [
+    {
+      strMeal: string;
+      strIngredient: string;
+      strMeasure: string;
+      strInstructions: string;
+    }
+  ];
+};
+
+function Search(): JSX.Element {
+  const [meals, setMeals] = useState<Meals | null>(null);
+
+  async function handleSearch(mealName: string) {
+    const response = await fetch(`/api/search/?mealName=${mealName}`);
+    const searchedMeals = await response.json();
+    setMeals(searchedMeals);
+  }
+  return (
+    <div className={styles.container}>
+      <NavBar />
+      <MealSearch onSubmit={handleSearch} />
+      {meals?.meals.map((meal) => {
+        return <a>{meal.strMeal}</a>;
+      })}
+    </div>
+  );
+}
+
+export default Search;
