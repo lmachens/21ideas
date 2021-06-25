077c726404052516386c1b929ffed58bd4ae75a4
J-Bannach <JohannaBannach@gmail.com>
2021-06-25 17:21:26 +0200

    ✨ Create Selection block component

---
+++ src/app/components/SelectionBlock/SelectionBlock.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import MealSelection from '../MealSelection/MealSelection';
+import styles from './SelectionBlock.module.css';
+
+function SelectionBlock(): JSX.Element {
+  return (
+    <div className={styles.selectionBlock}>
+      <MealSelection link="./search" />
+      <MealSelection link="./search" />
+      <MealSelection link="./search" />
+    </div>
+  );
+}
+
+export default SelectionBlock;
