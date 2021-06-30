3b0f402728cc6856cbd88891cf35dc4bfa3aa751
J-Bannach <JohannaBannach@gmail.com>
2021-06-30 15:37:17 +0200

    🏷️  Update types

--- src/types.ts
+++ src/types.ts
@@ -16,8 +16,30 @@ export type Meal = {
 };
 
 export type Recipe = {
-  id: number;
-  headline: string;
+  id: string;
+  name: string;
   ingredients: string;
   instructions: string;
+};
+
+export type Meals = {
+  idMeal: string;
+  strMeal: string;
+  strIngredient1: string;
+  strIngredient2: string;
+  strIngredient3: string;
+  strIngredient4: string;
+  strIngredient5: string;
+  strIngredient6: string;
+  strIngredient7: string;
+  strIngredient8: string;
+  strMeasure1: string;
+  strMeasure2: string;
+  strMeasure3: string;
+  strMeasure4: string;
+  strMeasure5: string;
+  strMeasure6: string;
+  strMeasure7: string;
+  strMeasure8: string;
+  strInstructions: string;
 };
