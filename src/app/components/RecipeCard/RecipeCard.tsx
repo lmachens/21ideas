1030fbefdb649723272a7c87ebfd5824d11bb490
J-Bannach <JohannaBannach@gmail.com>
2021-06-30 15:36:57 +0200

    ⚡️  Make recipe page dynamic

--- src/app/components/RecipeCard/RecipeCard.tsx
+++ src/app/components/RecipeCard/RecipeCard.tsx
@@ -2,21 +2,70 @@ import React from 'react';
 import styles from './RecipeCard.module.css';
 
 type RecipeCardProps = {
-  headline: string;
-  ingredients: string;
-  instructions: string;
+  strMeal: string;
+  strIngredient1: string;
+  strIngredient2: string;
+  strIngredient3: string;
+  strIngredient4: string;
+  strIngredient5: string;
+  strIngredient6: string;
+  strIngredient7: string;
+  strIngredient8: string;
+  strMeasure1: string;
+  strMeasure2: string;
+  strMeasure3: string;
+  strMeasure4: string;
+  strMeasure5: string;
+  strMeasure6: string;
+  strMeasure7: string;
+  strMeasure8: string;
+  strInstructions: string;
 };
 
 function RecipeCard({
-  headline,
-  ingredients,
-  instructions,
+  strMeal,
+  strIngredient1,
+  strIngredient2,
+  strIngredient3,
+  strIngredient4,
+  strIngredient5,
+  strIngredient6,
+  strIngredient7,
+  strIngredient8,
+  strMeasure1,
+  strMeasure2,
+  strMeasure3,
+  strMeasure4,
+  strMeasure5,
+  strMeasure6,
+  strMeasure7,
+  strMeasure8,
+  strInstructions,
 }: RecipeCardProps): JSX.Element {
   return (
     <div className={styles.container}>
-      <h3 className={styles.headline}>{headline}</h3>
-      <p className={styles.ingredients}>{ingredients}</p>
-      <p className={styles.instructions}>{instructions}</p>
+      <h3 className={styles.headline}>{strMeal}</h3>
+      <div className={styles.ingredients}>
+        <p className={styles.ingredients}>{strIngredient1}</p>
+        <p className={styles.ingredients}>{strIngredient2}</p>
+        <p className={styles.ingredients}>{strIngredient3}</p>
+        <p className={styles.ingredients}>{strIngredient4}</p>
+        <p className={styles.ingredients}>{strIngredient5}</p>
+        <p className={styles.ingredients}>{strIngredient6}</p>
+        <p className={styles.ingredients}>{strIngredient7}</p>
+        <p className={styles.ingredients}>{strIngredient8}</p>
+      </div>
+      <div className={styles.measures}>
+        <p className={styles.ingredients}>{strMeasure1}</p>
+        <p className={styles.ingredients}>{strMeasure2}</p>
+        <p className={styles.ingredients}>{strMeasure3}</p>
+        <p className={styles.ingredients}>{strMeasure4}</p>
+        <p className={styles.ingredients}>{strMeasure5}</p>
+        <p className={styles.ingredients}>{strMeasure6}</p>
+        <p className={styles.ingredients}>{strMeasure7}</p>
+        <p className={styles.ingredients}>{strMeasure8}</p>
+      </div>
+      <p className={styles.instructions}>{strInstructions}</p>
     </div>
   );
 }
--- src/app/pages/RecipePage/RecipePage.tsx
+++ src/app/pages/RecipePage/RecipePage.tsx
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import NavBar from '../../components/NavBar/NavBar';
 import RecipeCard from '../../components/RecipeCard/RecipeCard';
-import { Recipe } from '../../../types';
+import { Meals } from '../../../types';
 import styles from './RecipePage.module.css';
 
 function RecipePage(): JSX.Element {
-  const [recipes, setRecipes] = useState<Recipe[]>([]);
+  const [recipes, setRecipes] = useState<Meals[]>([]);
 
   useEffect(() => {
     fetch('/api/recipes')
       .then((response) => response.json())
       .then((recipes) => setRecipes(recipes));
   }, []);
+  console.log(recipes);
 
   return (
     <div className={styles.container}>
@@ -21,10 +22,25 @@ function RecipePage(): JSX.Element {
       <main className={styles.main}>
         {recipes.map((recipe) => (
           <RecipeCard
-            key={recipe.id}
-            headline={recipe.headline}
-            ingredients={recipe.ingredients}
-            instructions={recipe.instructions}
+            key={recipe.idMeal}
+            strMeal={recipe.strMeal}
+            strIngredient1={recipe.strIngredient1}
+            strIngredient2={recipe.strIngredient2}
+            strIngredient3={recipe.strIngredient3}
+            strIngredient4={recipe.strIngredient4}
+            strIngredient5={recipe.strIngredient5}
+            strIngredient6={recipe.strIngredient6}
+            strIngredient7={recipe.strIngredient7}
+            strIngredient8={recipe.strIngredient8}
+            strMeasure1={recipe.strMeasure1}
+            strMeasure2={recipe.strMeasure2}
+            strMeasure3={recipe.strMeasure3}
+            strMeasure4={recipe.strMeasure4}
+            strMeasure5={recipe.strMeasure5}
+            strMeasure6={recipe.strMeasure6}
+            strMeasure7={recipe.strMeasure7}
+            strMeasure8={recipe.strMeasure8}
+            strInstructions={recipe.strInstructions}
           />
         ))}
       </main>
