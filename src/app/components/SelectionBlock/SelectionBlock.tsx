c046139f3e77849481d18f7af3dbceab2b89e8c6
J-Bannach <JohannaBannach@gmail.com>
2021-06-30 17:02:15 +0200

    ⚡️ Implement local storage

--- src/app/components/SelectionBlock/SelectionBlock.tsx
+++ src/app/components/SelectionBlock/SelectionBlock.tsx
@@ -6,39 +6,39 @@ function SelectionBlock(): JSX.Element {
   return (
     <div className={styles.selectionBlock}>
       <div>
-        <MealSelection link="./search" />
-        <MealSelection link="./search" />
-        <MealSelection link="./search" />
+        <MealSelection link="/search?day=monday&time=breakfast" />
+        <MealSelection link="/search?day=monday&time=lunch" />
+        <MealSelection link="/search?day=monday&time=dinner" />
       </div>
       <div>
-        <MealSelection link="./search" />
-        <MealSelection link="./search" />
-        <MealSelection link="./search" />
+        <MealSelection link="/search?day=tuesday&time=breakfast" />
+        <MealSelection link="/search?day=tuesday&time=lunch" />
+        <MealSelection link="/search?day=tuesday&time=dinner" />
       </div>
       <div>
-        <MealSelection link="./search" />
-        <MealSelection link="./search" />
-        <MealSelection link="./search" />
+        <MealSelection link="/search?day=wednesday&time=breakfast" />
+        <MealSelection link="/search?day=wednesday&time=lunch" />
+        <MealSelection link="/search?day=wednesday&time=dinner" />
       </div>
       <div>
-        <MealSelection link="./search" />
-        <MealSelection link="./search" />
-        <MealSelection link="./search" />
+        <MealSelection link="/search?day=thursday&time=breakfast" />
+        <MealSelection link="/search?day=thursday&time=lunch" />
+        <MealSelection link="/search?day=thursday&time=dinner" />
       </div>
       <div>
-        <MealSelection link="./search" />
-        <MealSelection link="./search" />
-        <MealSelection link="./search" />
+        <MealSelection link="/search?day=friday&time=breakfast" />
+        <MealSelection link="/search?day=friday&time=lunch" />
+        <MealSelection link="/search?day=friday&time=dinner" />
       </div>
       <div>
-        <MealSelection link="./search" />
-        <MealSelection link="./search" />
-        <MealSelection link="./search" />
+        <MealSelection link="/search?day=saturday&time=breakfast" />
+        <MealSelection link="/search?day=saturday&time=lunch" />
+        <MealSelection link="/search?day=saturday&time=dinner" />
       </div>
       <div>
-        <MealSelection link="./search" />
-        <MealSelection link="./search" />
-        <MealSelection link="./search" />
+        <MealSelection link="/search?day=sunday&time=breakfast" />
+        <MealSelection link="/search?day=sunday&time=lunch" />
+        <MealSelection link="/search?day=sunday&time=dinner" />
       </div>
     </div>
   );
--- src/app/pages/SearchPage/Search.tsx
+++ src/app/pages/SearchPage/Search.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import NavBar from '../../components/NavBar/NavBar';
 import styles from './Search.module.css';
 import MealSearch from '../../components/MealSearch/MealSearch';
+import { useLocalStorage } from '../../hooks/useLocalStorage';
+import { useLocation } from 'react-router-dom';
 
 type Meals = {
   meals: [
@@ -14,8 +16,29 @@ type Meals = {
   ];
 };
 
+type DayPlan = {
+  breakfast?: string;
+  lunch?: string;
+  dinner?: string;
+};
+type MealPlan = {
+  monday?: DayPlan;
+  tuesday?: DayPlan;
+  wednesday?: DayPlan;
+  thursday?: DayPlan;
+  friday?: DayPlan;
+  saturday?: DayPlan;
+  sunday?: DayPlan;
+};
+
 export function Search(): JSX.Element {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const day = searchParams.get('day');
+  const time = searchParams.get('time');
+
   const [meals, setMeals] = useState<Meals | null>(null);
+  const [mealPlan, setMealPlan] = useLocalStorage<MealPlan>('recipes', {});
 
   async function handleSearch(mealName: string) {
     const response = await fetch(`/api/search/?mealName=${mealName}`);
@@ -23,36 +46,43 @@ export function Search(): JSX.Element {
     setMeals(searchedMeals);
   }
 
-  function handleClick() {
-    function parseChoiceFromLocalStorage() {
-      const json = localStorage.getItem('recipe');
-      if (json === null) {
-        return {};
+  function selectMealName(mealName: string) {
+    const newMealPlan = { ...mealPlan };
+    if (
+      day === 'monday' ||
+      day === 'tuesday' ||
+      day === 'wednesday' ||
+      day === 'thursday' ||
+      day === 'friday' ||
+      day === 'saturday' ||
+      day === 'sunday'
+    ) {
+      if (time === 'breakfast' || time === 'lunch' || time === 'dinner') {
+        newMealPlan[day] = {
+          ...newMealPlan[day],
+          [time]: mealName,
+        };
+        setMealPlan(newMealPlan);
       }
-      const data = JSON.parse(json);
-      return data;
     }
-
-    const recipe = parseChoiceFromLocalStorage();
-
-    if (meals) {
-      recipe[meals] = { name: mealName };
-    }
-
-    localStorage.setItem('recipe', JSON.stringify(recipe));
-
-    return (
-      <div className={styles.container}>
-        <NavBar />
-        <MealSearch onSubmit={handleSearch} />
-        {meals?.meals.map((meal) => {
-          return (
-            <div className={styles.container__results}>
-              <a onClick={handleClick}>{meal.strMeal}</a>
-            </div>
-          );
-        })}
-      </div>
-    );
   }
+
+  return (
+    <div className={styles.container}>
+      <NavBar />
+      <MealSearch onSubmit={handleSearch} />
+      {meals?.meals.map((meal) => {
+        return (
+          <button
+            key={meal.strMeal}
+            onClick={() => selectMealName(meal.strMeal)}
+          >
+            {meal.strMeal}
+          </button>
+        );
+      })}
+    </div>
+  );
 }
+
+export default Search;
